4d259bbfa46b70b420fe3e00beb1fc2e48533c63
Antoni Boucher <bouanto@zoho.com>
2018-05-18 12:42:21 -0400

    Update configuration

--- offlineimap/.config/offlineimap/offlineimap.py
+++ offlineimap/.config/offlineimap/offlineimap.py
@@ -2,4 +2,4 @@ import subprocess
 from subprocess import check_output
 
 def get_pass(account):
-    return check_output("pass mail/" + account, shell=True, stderr=subprocess.STDOUT).splitlines()[0].decode("utf-8")
+    return check_output("gopass show -f mail/" + account, shell=True, stderr=subprocess.STDOUT).splitlines()[0].decode("utf-8")
