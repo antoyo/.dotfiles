55ce37b9913cde58c06f0b85811ad0cec2d299ef
Antoni Boucher <bouanto@zoho.com>
2022-07-21 10:08:10 -0400

    Update config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,5 @@
 zoxide init fish | source
 
-function increment
-    set line (commandline)
-    set episode (string match -r "\d+" $line)
-    set next (math $episode + 1)
-    set next (printf %02d $next)
-    set next_episode (string replace $episode "$next" $line)
-    commandline $next_episode
-end
-
-function fish_user_key_bindings
-    bind \es 'increment'
-end
-
 if set -q SSH_CLIENT; or set -q SSH_TTY
     source /usr/share/fish/tools/web_config/sample_prompts/classic_status.fish
 else
@@ -21,10 +8,6 @@ end
 
 function bsixdec
     echo "$argv[1]" | base64 -d
-end
-
-function add_event
-    ics2rem $argv[1] >> ~/.config/remind/reminders.rem
 end
 
 function z
@@ -88,20 +71,15 @@ alias helixconfig "cd ~/.config/helix; helix config.toml"
 alias i3config "nvim ~/.config/i3/config"
 alias ls "ls --color=auto -N"
 alias mkdir "mkdir -p"
-alias mount "udisksctl mount -b"
-alias mount_cd "udisksctl mount -b /dev/sr0"
 alias mpl "mpv --video-aspect-override=16:9"
 alias mv "mv -i"
 alias objdump "objdump -M intel-syntax"
 alias nvimconfig "cd ~/.config/nvim; nvim init.vim"
 alias perf-annotate "perf annotate -M intel"
-alias reminders "nvim ~/.config/remind/reminders.rem"
 alias rm "rm -i"
 alias stow "stow --no-folding"
 alias swayconfig "nvim ~/.config/sway/config"
 alias titaniumconfig "cd ~/.config/titanium; nvim config keys"
-alias unmount "udisksctl unmount -b"
-alias unmount_cd "udisksctl unmount -b /dev/sr0"
 alias urlsnews "nvim ~/.config/newsboat/urls"
 alias you "youtube-dl --no-playlist"
 alias mp3you "youtube-dl --ignore-errors --extract-audio --audio-format mp3 -o '%(title)s.%(ext)s'"
--- gdb/.gdbinit
+++ gdb/.gdbinit
@@ -1,6 +1,8 @@
 set disassembly-flavor intel
 set disassemble-next-line on
+set debuginfod enabled off
 set history save on
 define all_bt
     thread apply all bt
 end
+add-auto-load-safe-path /home/bouanto/Ordinateur/Programmation/Projets/linux-cg-gcc
