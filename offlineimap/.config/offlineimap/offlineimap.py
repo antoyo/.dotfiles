6ff0a948c247836d4f287af449ec22edff189205
Antoni Boucher <bouanto@zoho.com>
2024-04-05 09:40:55 -0400

    Cleanup: remove helix, i3-workscreen, imv, msmtp, mutt, neomutt, newsboat, offlineimap, sway, swaylock, swaync, teiler, termite, torrench, waybar and yay

--- offlineimap/.config/offlineimap/offlineimap.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-from subprocess import check_output
-
-def get_pass(account):
-    return check_output("gopass show -f mail/" + account, shell=True, stderr=subprocess.STDOUT).splitlines()[0].decode("utf-8")
